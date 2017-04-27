ad17682f0cfaa98e33be3e0b2484b92668d4d087
buuug7 <youpp@126.com>
2017-04-27 18:34:13 +0800

    add new theme wy-theme-v2

---
+++ themes/wy-theme-v2/assets/script/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    //电商咨询左侧悬浮显示
+    $('#afix-lanmu').affix({
+        offset: {
+            top: 31 + 200 + 40 + 52,
+            bottom: 210 + 1,
+        },
+    });
+    $("#lt-002").slide({});
+
+}); // ready end
+
