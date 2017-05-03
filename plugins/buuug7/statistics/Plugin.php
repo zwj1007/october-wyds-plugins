5b74ec3a28e4cc8df59b8c18b953f3a9ce30a781
buuug7 <youpp@126.com>
2017-05-03 18:57:32 +0800

    Update Plugin.php

--- plugins/buuug7/statistics/Plugin.php
+++ plugins/buuug7/statistics/Plugin.php
@@ -50,10 +50,8 @@ class Plugin extends PluginBase
      */
     public function registerComponents()
     {
-        return []; // Remove this line to activate
-
         return [
-            'Buuug7\Statistics\Components\MyComponent' => 'myComponent',
+            ' Buuug7\Statistics\Components\Statistics'=>'b7Statistics',
         ];
     }
 
