8ff532857604987f71191856e5bc29d5af66743f
buuug7 <youpp@126.com>
2017-05-03 23:21:30 +0800

    Update seed_all_tables.php

--- plugins/buuug7/statistics/updates/seed_all_tables.php
+++ plugins/buuug7/statistics/updates/seed_all_tables.php
@@ -14,9 +14,9 @@ class SeedAllTables extends Seeder
             'summary' => '2016上半年代购统计',
             'published' => true,
             'published_at' => Carbon::now(),
-            'detail' => [
-              ["name"=>"胡萝卜","price"=>"0.87","quantity"=>"10000","total"=>""]
-            ]
+            'detail' => {
+                1:{"name"=>"胡萝卜","price"=>"0.87","quantity"=>"10000","total"=>""}
+            }
         ]);
         
     }
