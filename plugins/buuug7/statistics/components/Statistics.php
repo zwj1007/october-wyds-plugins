02e3a169805fdcb18277bc39be2dfb463a732284
buuug7 <youpp@126.com>
2017-05-03 18:46:31 +0800

    Create Statistics.php

---
+++ plugins/buuug7/statistics/components/Statistics.php
@@ -0,0 +1,33 @@
+<?php namespace Buuug7\Statistics\Components;
+use Cms\Classes\ComponentBase;
+use Buuug7\Courses\Models\Course;
+
+class Statistics extends ComponentBase
+{
+    public $statistics;
+    public function componentDetails()
+    {
+        return [
+            'name' => 'Statistics Component',
+            'description' => 'No description provided yet...'
+        ];
+    }
+    public function defineProperties()
+    {
+        return [];
+    }
+    public function onRun()
+    {
+        $this->statistics = $this->page['statistics'] = $this->listStatistics();
+    }
+    public function listStatistics()
+    {
+        $check = Auth::check();
+        if (!$check) {
+            return null;
+        }
+        $user = Auth::getUser();
+        $courses = $user->courses();
+        return $courses->paginate(2);
+    }
+}
