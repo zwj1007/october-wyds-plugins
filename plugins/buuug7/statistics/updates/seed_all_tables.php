54a84f37b304fb26b3456b970732be650a117bd2
buuug7 <youpp@126.com>
2017-05-03 23:55:43 +0800

    Update seed_all_tables.php

--- plugins/buuug7/statistics/updates/seed_all_tables.php
+++ plugins/buuug7/statistics/updates/seed_all_tables.php
@@ -25,5 +25,22 @@ class SeedAllTables extends Seeder
             ]
         ]);
         
+        
+        $post01=Statistic::create([
+            'title' => '2016上半年代销统计',
+            'summary' => '2016上半年代销统计',
+            'published' => true,
+            'published_at' => Carbon::now(),
+            'detail' => [
+                1=>["name"=>"白菜","price"=>"0.78","quantity"=>"80500","total"=>""],
+                2=>["name"=>"白萝卜","price"=>"0.87","quantity"=>"63087","total"=>""],
+                3=>["name"=>"红萝卜","price"=>"1.14","quantity"=>"33102","total"=>""],
+                4=>["name"=>"大蒜","price"=>"3.15","quantity"=>"55941","total"=>""],
+                5=>["name"=>"生姜","price"=>"3.67","quantity"=>"32176","total"=>""],
+                6=>["name"=>"青椒","price"=>"1.32","quantity"=>"20447","total"=>""],
+                7=>["name"=>"马铃薯","price"=>"0.61","quantity"=>"2187099","total"=>""]
+            ]
+        ]);
+        
     }
 }
