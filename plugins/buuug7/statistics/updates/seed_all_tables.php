6c4bfa424fb5945dccae2f29dcd5260a36e074ba
buuug7 <youpp@126.com>
2017-05-03 23:53:52 +0800

    Update seed_all_tables.php

--- plugins/buuug7/statistics/updates/seed_all_tables.php
+++ plugins/buuug7/statistics/updates/seed_all_tables.php
@@ -16,15 +16,12 @@ class SeedAllTables extends Seeder
             'published_at' => Carbon::now(),
             'detail' => [
                 1=>["name"=>"白菜","price"=>"0.78","quantity"=>"89500","total"=>""],
-                2=>["name"=>"白菜","price"=>"0.78","quantity"=>"89500","total"=>""],
-                3=>["name"=>"白菜","price"=>"0.78","quantity"=>"89500","total"=>""],
-                4=>["name"=>"白菜","price"=>"0.78","quantity"=>"89500","total"=>""],
-                5=>["name"=>"白菜","price"=>"0.78","quantity"=>"89500","total"=>""],
-                6=>["name"=>"白菜","price"=>"0.78","quantity"=>"89500","total"=>""],
-                7=>["name"=>"白菜","price"=>"0.78","quantity"=>"89500","total"=>""],
-                8=>["name"=>"白菜","price"=>"0.78","quantity"=>"89500","total"=>""],
-                9=>["name"=>"白菜","price"=>"0.78","quantity"=>"89500","total"=>""],
-                10=>["name"=>"白菜","price"=>"0.78","quantity"=>"89500","total"=>""],
+                2=>["name"=>"白萝卜","price"=>"0.87","quantity"=>"73087","total"=>""],
+                3=>["name"=>"红萝卜","price"=>"1.14","quantity"=>"43102","total"=>""],
+                4=>["name"=>"大蒜","price"=>"3.15","quantity"=>"65941","total"=>""],
+                5=>["name"=>"生姜","price"=>"3.67","quantity"=>"34176","total"=>""],
+                6=>["name"=>"青椒","price"=>"1.32","quantity"=>"20947","total"=>""],
+                7=>["name"=>"马铃薯","price"=>"0.61","quantity"=>"2197099","total"=>""]
             ]
         ]);
         
