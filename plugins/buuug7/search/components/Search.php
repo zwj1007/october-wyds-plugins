0e90f23b547a71dbb130cbc5c4f3f71e823c92da
buuug7 <youpp@126.com>
2017-05-24 10:00:06 +0000

    update ...

--- plugins/buuug7/search/components/Search.php
+++ plugins/buuug7/search/components/Search.php
@@ -11,26 +11,35 @@ class Search extends ComponentBase
     public function componentDetails()
     {
         return [
-            'name'        => 'Search Component',
+            'name' => 'Search Component',
             'description' => 'No description provided yet...'
         ];
     }
 
 
-    public function onSearch(){
-        $data=post();
-        
-        $rules=[
+    public function onSearch()
+    {
+        $data = post();
+
+        $rules = [
             'search' => 'required',
         ];
 
-        $validation=Validator::make($data,$rules);
-        if($validation->fails()){
+        $validation = Validator::make($data, $rules);
+        if ($validation->fails()) {
             throw new ValidationException($validation);
-        }else{
-            $category=post('category');
-            $search=post('search');
-            return Redirect::to('/news/posts?search='.$search);
+        } else {
+            $category = post('category');
+            $search = post('search');
+            if ($category == 'news') {
+                return Redirect::to('/news/posts?search=' . $search);
+            } elseif ($category == 'course') {
+                return Redirect::to('/coming-soon');
+            } elseif ($category == 'need') {
+                return Redirect::to('/coming-soon');
+            } else {
+                return Redirect::to('/404');
+            }
         }
 
         Log::info($data);
