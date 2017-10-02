11f0a0b163f8d5096e0a1f4fd04c7f9e739e4881
buuug7 <youpp@126.com>
2017-10-02 01:17:12 +0000

    fix

--- plugins/buuug7/user/config/production/config.php
+++ plugins/buuug7/user/config/production/config.php
@@ -12,5 +12,22 @@ return [
         'client_secret' => 'BzXQ9q5G9kdYYsUuvjEFUOMw1V56cQt1H4EEcLj2',
         'redirect_uri' => 'http://6.jq2.com/login/tianqi/callback',
     ],
+    'packages' => [
+        'jenssegers/agent' => [
+            // Service providers to be registered by your plugin
+            'providers' => [
+                \Jenssegers\Agent\AgentServiceProvider::class,
+            ],
+
+            // Aliases to be registered by your plugin in the form of $alias => $pathToFacade
+            'aliases' => [
+                'Agent' => Jenssegers\Agent\Facades\Agent::class,
+            ],
+
+            // The namespace to set the configuration under. For this example, this package accesses it's config via config('purifier.' . $key), so the namespace 'purifier' is what we put here
+            'config_namespace' => 'agent',
+
+        ],
+    ],
 ];
 
