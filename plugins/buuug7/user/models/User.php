80f1e81887817a623945ffcb065316eabf3bd8c0
buuug7 <youpp@126.com>
2017-12-06 00:50:47 +0800

    add qq auth

--- plugins/buuug7/user/models/User.php
+++ plugins/buuug7/user/models/User.php
@@ -5,6 +5,50 @@ use October\Rain\Support\Facades\Config;
 
 class User extends \RainLab\User\Models\User
 {
+    //
+    // qq
+    //
+    public static function getQQUserToken($code)
+    {
+        $http = new Client();
+        $response = $http->post('https://graph.qq.com/oauth2.0/token', [
+            'form_params' => [
+                'grant_type' => 'authorization_code',
+                'client_id' => '101441921',
+                'client_secret' => '0b51fea80164a1c697b8e1663f1d9da2',
+                'redirect_uri' => 'http://ds8.com.cn/login/qq/callback',
+                'code' => $code,
+            ],
+        ]);
+        $accessToken = json_decode($response->getBody())->access_token;
+        return $accessToken;
+    }
+
+    public static function getQQUserOpenId($token)
+    {
+        $openID_API='https://graph.qq.com/oauth2.0/me';
+        $http = new Client();
+        $response = $http->get($openID_API,[
+            'form_params' => [
+                'access_token' => $token,
+            ],
+        ]);
+        return json_encode($response->getBody());
+    }
+
+    public static function getQQUser($token,$appID,$openID)
+    {
+        $userApi = 'https://graph.qq.com/user/get_user_info';
+        $http = new Client();
+        $response = $http->get($userApi, [
+            'form_params' => [
+                'access_token' => $token,
+                'oauth_consumer_key' => $appID,
+                'openid' => $openID,
+            ],
+        ]);
+        return json_decode($response->getBody());
+    }
 
     public static function getTianQiUserToken($code)
     {
--- plugins/buuug7/user/routes.php
+++ plugins/buuug7/user/routes.php
@@ -11,10 +11,10 @@ use Illuminate\Support\Str;
 
 Route::middleware(['web'])->group(function (){
     //
-// login with tianqi user center
-//
+    // login with tianqi user center
+    //
 
-// redirect to tianqi login
+    // redirect to tianqi login
     Route::get('/login/tianqi', function () {
         $query = http_build_query([
             'client_id' => Config::get('buuug7.user::tianqi.client_id'),
@@ -27,7 +27,7 @@ Route::middleware(['web'])->group(function (){
         return Redirect::to($authorize_uri);
     });
 
-// handle tianqi callback
+    // handle tianqi callback
     Route::get('/login/tianqi/callback', function () {
         $code = input('code');
         $accessToken = User::getTianQiUserToken($code);
@@ -66,11 +66,11 @@ Route::middleware(['web'])->group(function (){
 
 
 
-//
-// login with github
-//
+    //
+    // login with github
+    //
 
-// redirect to github login
+    // redirect to github login
     Route::get('/login/github', function () {
         $query = http_build_query([
             'client_id' => Config::get('buuug7.user::github.client_id'),
@@ -81,7 +81,7 @@ Route::middleware(['web'])->group(function (){
         return Redirect::to($authorize_uri);
     });
 
-// handle github callback
+    // handle github callback
     Route::get('/login/github/callback', function () {
         // exchange access_token with code
         // get github user info
@@ -126,33 +126,77 @@ Route::middleware(['web'])->group(function (){
     });
 
 
-//
-// login with qq
-//
+    //
+    // login with qq
+    //
 
-// redirect to qq login
+    // redirect to qq login
     Route::get('/login/qq', function () {
-        return Redirect::to('/coming-soon');
+        $query = http_build_query([
+            'response_type' => 'code',
+            'client_id' => '101441921',
+            'redirect_uri' => 'http://ds8.com.cn/login/qq/callback',
+            'state' => 'qqlinginstate',
+            'scope' => 'get_user_info',
+        ]);
+        $authorize_uri = 'https://graph.qq.com/oauth2.0/authorize?' . $query;
+        return Redirect::to($authorize_uri);
     });
-// handle qq callback
+    // handle qq callback
     Route::get('login/qq/callback', function () {
-        return Redirect::to('/coming-soon');
+        $code = input('code');
+        $accessToken = User::getQQUserToken($code);
+
+        $appID = User::getQQUserOpenId($accessToken)->client_id;
+        $openID = User::getQQUserOpenId($accessToken)->openid;
+
+        $qqUser = User::getQQUser($accessToken,$appID,$openID);
+
+        return $qqUser;
+
+        if (!User::where('qq_id', $qqUser->id)->first()) {
+            if (User::where('email', $qqUser->email)->first()) {
+                $user = User::where('email', $qqUser->email)->first();
+                $user->tianqi_id = $qqUser->id;
+                $user->social_avatar = $qqUser->avatar_url;
+                $user->save();
+                Auth::login($user);
+            } else {
+                $password = bcrypt(Str::random(6));
+                $user = Auth::register([
+                    'name' => $qqUser->name,
+                    'email' => $qqUser->email,
+                    'password' => $password,
+                    'password_confirmation' => $password,
+                ], true);
+                $user->tianqi_id = $qqUser->id;
+                $user->social_avatar = $qqUser->avatar_url;
+                $user->save();
+                Auth::login($user);
+            }
+        } else {
+            $userInstance = User::where('qq_id', $qqUser->id)->firstOrFail();
+            // synchronization user avatar
+            $userInstance->social_avatar = $qqUser->avatar_url;
+            $userInstance->save();
+            Auth::login($userInstance, true);
+
+        }
     });
 
 
-//
-// login with weixin
-//
+    //
+    // login with weixin
+    //
 
-// redirect to weixin login
+    // redirect to weixin login
     Route::get('login/weixin', function () {
         return Redirect::to('/coming-soon');
     });
-// handle weixin callback
+    // handle weixin callback
     Route::get('login/weixin/callback', function () {
         return Redirect::to('/coming-soon');
     });
-
 
 
 });
